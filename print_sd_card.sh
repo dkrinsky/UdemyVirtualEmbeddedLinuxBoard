65e699fe37f7933b295be95b4e46ace093ddd1dd
DAVID KRINSKY <dkrinsk@gmail.com>
2024-02-15 16:19:47 -0500

    fix filename in sdcard print script

--- print_sd_card.sh
+++ print_sd_card.sh
@@ -1,5 +1,5 @@
 # use fdisk print to show sdcard partitions
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk sdcardtest.img
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk sdcard.img
   p # print the in-memory partition table
   q # and we're done
 EOF

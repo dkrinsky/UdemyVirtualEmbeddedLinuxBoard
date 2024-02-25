80904f0caec2346ec9941b7813581c41e2be7efa
DAVID KRINSKY <dkrinsk@gmail.com>
2024-02-25 07:36:48 -0500

    update get scripts for busybox and kernel

--- get_busybox.sh
+++ get_busybox.sh
@@ -1,12 +1,24 @@
 mkdir -p busybox
+rm -rf busybox
+mkdir -p busybox
+
 cd busybox
 wget https://busybox.net/downloads/busybox-1.36.0.tar.bz2
 tar xvf busybox-1.36.0.tar.bz2
 cd busybox-1.36.0
 
-export ARCH=arm
-export CROSS_COMPILE=arm-linux-
-make defconfig
+# there is no make savedefconfig support in busybox
+# therefore, the .config file generated from the make menuconfig
+# (e.g. disables IPV6) is checked into the project directly.
+# using make defconfig will undo some required make menuconfig commands
+# and cause compile errors.
+# bottom line: just make using checked in .config
 
-#make menuconfig
+git restore .config
+
+CROSS_COMPILE=arm-linux- make
+
+cd ../../
+
+
 
---
+++ get_kernel.sh
@@ -0,0 +1,14 @@
+mkdir -p kernel
+cd kernel
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.11.tar.xz
+tar xvf linux-6.6.11.tar.xz
+cd linux-6.6.11
+
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-
+# no additional menuconfig changes are required,
+#so just use checked in defconfig
+make vexpress_defconfig
+make
+
+

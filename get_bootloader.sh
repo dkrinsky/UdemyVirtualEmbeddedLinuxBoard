21a9cbd69025ebf0c64b23a926d0c76cf32e3eba
DAVID KRINSKY <dkrinsk@gmail.com>
2024-02-27 17:23:45 -0500

    update top.sh to get, build, and run uboot->kernel->rootfs from sdcard

--- get_bootloader.sh
+++ get_bootloader.sh
@@ -6,7 +6,7 @@ tar xvf u-boot-2023.10.tar.bz2
 cd u-boot-2023.10
 
 # patch include/configs/vexpress_common.h to set boot.scr load address
-patch -p1 < vexpress_common.patch
+patch -p1 < ../../vexpress_common.patch
 
 #compile u-boot
 export ARCH=arm
--- top.sh
+++ top.sh
@@ -1,5 +1,39 @@
-bash create_rootfs_ext4.sh
-bash set_rootfs_users.sh
 
-#run_kernel_rootfs
-qemu-system-arm -M vexpress-a9 -kernel kernel/linux-6.6.11/arch/arm/boot/zImage -dtb kernel/linux-6.6.11/arch/arm/boot/dts/arm/vexpress-v2p-ca9.dtb -append 'console=ttyAMA0,115200 root=/dev/mmcblk0 rw' -nographic -sd rootfs.ext4
+# setup convenient aliases 
+. setup_alias.sh
+
+# wget, patch, and build u-boot
+# patch updates include/configs/vexpress_common.h to set boot.scr load address
+. get_bootloader.sh
+
+# create and display empty sd_card
+. create_sd_card.sh
+. print_sd_card.sh
+
+# wget and build kernel
+. get_kernel.sh
+
+# wget, build,and install busybox
+. get_busybox.sh
+
+# create rootfs.ext4 using busybox
+# 
+. create_rootfs_ext4.sh
+
+# load sd_card FAT partition p1 with kernel, dtb
+# load sd_card EXT4 partition p2 with rootfs.ext4
+# display sd_card for sanity check
+
+. load_sd_card.sh
+. print_sd_card.sh
+
+
+# make boot.scr (from boot.txt) and add to FAT (boot) parition1 of sd card
+
+mk_bootscr
+. add_to_sd_card.sh 1 boot.scr
+
+#run
+run_uboot_sdcard
+
+

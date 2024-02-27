05cb87b27ec4167158aa014161e0e28c09dc08ec
DAVID KRINSKY <dkrinsk@gmail.com>
2024-02-27 17:00:05 -0500

    add patch for vexpress_common to set boot.scr load addr

--- get_bootloader.sh
+++ get_bootloader.sh
@@ -5,6 +5,10 @@ wget https://ftp.denx.de/pub/u-boot/u-boot-2023.10.tar.bz2
 tar xvf u-boot-2023.10.tar.bz2
 cd u-boot-2023.10
 
+# patch include/configs/vexpress_common.h to set boot.scr load address
+patch -p1 < vexpress_common.patch
+
+#compile u-boot
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-
 make udemy_qemu_vm_defconfig
